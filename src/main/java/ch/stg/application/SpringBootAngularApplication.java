417f3b296a617eabfe3cbb9b2c66f92e2620390a
stephane <gui_steph@yahoo.fr>
2019-07-21 12:25:56 +0200

    Création du controller REST et du repository

--- src/main/java/ch/stg/application/SpringBootAngularApplication.java
+++ src/main/java/ch/stg/application/SpringBootAngularApplication.java
@@ -1,7 +1,14 @@
 package ch.stg.application;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import ch.stg.application.entities.User;
+import ch.stg.application.repositories.UserRepository;
 
 @SpringBootApplication
 public class SpringBootAngularApplication {
@@ -9,5 +16,16 @@ public class SpringBootAngularApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAngularApplication.class, args);
 	}
+	
+	@Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
 
 }
